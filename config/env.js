ea78fd0dedf479a07473e51fd39fa3278c664ca1
0xMAXLW <max.leon.weiss@gmail.com>
2024-09-12 12:55:40 -0700

    Remove config/env.js from tracking

--- config/env.js
+++
@@ -1,7 +0,0 @@
-const mongoURI = 'mongodb+srv://0xMAX:0xCAFEBABEDEADBEEF@learningcluster.3j9n3.mongodb.net';
-const secretKey = '618547c63082a48a0479088549c8474d00f7f62b11b889d6dc3d1c9e842d10d3';
-
-module.exports = {
-  mongoURI,
-  secretKey,
-};
